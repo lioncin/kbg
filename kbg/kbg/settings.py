cde4b642d141f0a25c343d828ed179142fe6ee1a
xing.lin <linxing1984@gmail.com>
2012-04-08 19:04:08 +0800

    delete database from sqlit to mysql

--- kbg/kbg/settings.py
+++ kbg/kbg/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:\\git\\kbg\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.

72b04f29a1fae9b1ce61963fbfeb652bcbbe86bd
xing.lin <linxing1984@gmail.com>
2012-05-26 00:36:25 +0800

    add new order view

--- kbg/kbg/order/views.py
+++ kbg/kbg/order/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.conf import settings
+from kbg.order.forms import OrderForm
 import json
 
 #add by xlin in 2012-5-25
@@ -10,4 +11,10 @@ def newOrder(request):
     context = {}
     context['settings'] = settings
     context['MEDIA_URL'] = settings.MEDIA_URL
-    return render_to_response('new_order.html',context)
+    if request.method == 'GET':
+        form = OrderForm(request['GET'])
+    else:
+        form = OrderForm(request['POST'])
+        
+    context['form'] = form
+    return render_to_response('new_order.html', context)
